baa265253b6e4385831d75e1eefaf16e677c829e
ClownNero <ClownNero@gmail.com>
2023-09-27 23:04:23 +0900

    second

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,10 @@
 import React from "react";
 
 export default function Home(props) {
-<<<<<<< HEAD
   return (
     <>
-      <div>Home1</div>
+      <div>Home2</div>
       <div></div>
     </>
   );
-=======
-    return <div>Home2</div>;
-    
->>>>>>> bf3fa2a4729332f7ef1ea472ff40a17b8e87681c
 }
