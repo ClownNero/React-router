08cc9cb302ebb91d3590595ad9b4f581fe8c9c67
ClownNero <ClownNero@gmail.com>
2023-09-27 23:32:03 +0900

    sfaf

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,12 @@ import React from "react";
 export default function Home(props) {
   return (
     <>
-      <div>hihihihi</div>
+      <div>hihihihi2</div>
       <div></div>
       <div></div>
+      <div></div>
+      <div></div>
+      <main>hi22</main>
     </>
   );
 }
